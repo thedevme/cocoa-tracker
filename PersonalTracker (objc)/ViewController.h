7ab405fc6a6b7bd05901463b80d650c6c1776283
Craig Clayton <craig@thedev.me>
2016-01-20 14:38:57 -0500

    storyboard setup

---
+++ PersonalTracker (objc)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PersonalTracker (objc)
+//
+//  Created by Craig Clayton on 1/20/16.
+//  Copyright © 2016 Cocoa Academy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PersonalTracker (objc)/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PersonalTracker (objc)
+//
+//  Created by Craig Clayton on 1/20/16.
+//  Copyright © 2016 Cocoa Academy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
